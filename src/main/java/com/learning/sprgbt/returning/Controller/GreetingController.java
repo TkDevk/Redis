0bec46402ff6f75ad41c85244c252e7368778aa4
TkDevk <duvanuriel@hotmail.com>
2024-05-31 00:31:41 -0500

    Sending Logger

--- src/main/java/com/learning/sprgbt/returning/Controller/GreetingController.java
+++ src/main/java/com/learning/sprgbt/returning/Controller/GreetingController.java
@@ -1,12 +1,17 @@
 package com.learning.sprgbt.returning.Controller;
 
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Slf4j
 @RestController
+//@Log4j2
 public class GreetingController {
     @GetMapping("/test")
     public String hello(){
+        log.info("Hello there");
         return "Hello World";
     }
+
 }
